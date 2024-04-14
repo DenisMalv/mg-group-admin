7156956bd2686e56d79ed597ffd09d3732f0d75c
DenisMalv <81264326+DenisMalv@users.noreply.github.com>
2024-04-14 11:11:36 +0000

    Deploying to gh-pages from @ DenisMalv/mg-group-admin@f31105030be0d366083767ae47b510c079cc60f3 🚀

--- static/js/145.c0f3446a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework=self.webpackChunkreact_homework||[]).push([[145],{145:(e,r,t)=>{t.r(r),t.d(r,{default:()=>c});t(504),t(184);const c=function(e){let{children:r}=e;return r}}}]);
-//# sourceMappingURL=145.c0f3446a.chunk.js.map
--- static/js/605.29740ad5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework=self.webpackChunkreact_homework||[]).push([[605],{605:(e,r,t)=>{t.r(r),t.d(r,{default:()=>c});t(871),t(184);const c=e=>{let{children:r}=e;return r}}}]);
-//# sourceMappingURL=605.29740ad5.chunk.js.map
